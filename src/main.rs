deb9ea4bfbaf213200aaa4aa20fa46dbc5d695eb
Zhiyuan Dai <daizhiyuan@phytium.com.cn>
2023-05-16 09:20:03 +0800

    refactor: use function call
    Signed-off-by: Zhiyuan Dai <daizhiyuan@phytium.com.cn>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use jep106::JEP106Code;
 
-fn main() {
+fn printManufacturer(this: JEP106Code) {
+    println!("Manufacturer is [{}]", this);
+}
 
-    println!("Manufacturer is [{}]", JEP106Code::new(0x04, 0x3b));
-    println!("Manufacturer is [{}]", JEP106Code::new(0x08, 0x19));
-    println!("Manufacturer is [{}]", JEP106Code::new(0x0, 0x1));
-    println!("Manufacturer is [{}]", JEP106Code::new(0x0, 0x49));
-    println!("Manufacturer is [{}]", JEP106Code::new(0x0, 0x70));
+fn main() {
+    printManufacturer(JEP106Code::new(0x4, 0x3b));
+    printManufacturer(JEP106Code::new(0x8, 0x19));
 }
